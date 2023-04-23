c191192bd3b5de1b1e15893d97ecf0d074ccfcaa
unique-red <noah13victor@gmail.com>
2023-04-23 21:03:01 +0100

    args

--- pthw/ex13.py
+++ pthw/ex13.py
@@ -1,8 +1,21 @@
-from sys import argv
+from sys import argv 
 
-script, first, second, third = argv
+script, user_name = argv
+prompt = '> '
 
-print("The script is called:", script)
-print("Your first variable is:", first)
-print("Your secon variable is:", second)
-print("Your third variable is:", third)
+print (f"Hi {user_name}, I'm the {script} script.")
+print ("I'd like to ask you a few questions.")
+print (f"Do you like me {user_name}?")
+likes = input(prompt)
+
+print (f"Where do you live {user_name}?")
+lives = input(prompt)
+
+print ("What kind of computer do you have?")
+computer = input(prompt)
+
+print (f"""
+Alright, so you said {likes} about liking me.
+You live in {lives}. Not sure where that is.
+And you have a {computer} computer. Nice.
+""")
